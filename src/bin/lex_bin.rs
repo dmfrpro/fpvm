b6f07dda8fb18efd8f349cd2f2557715ceac4753
Vladimir <karasev.vladimir.2000@gmail.com>
2026-02-28 19:01:20 +0300

    Handle stdin read errors

--- src/bin/lex_bin.rs
+++ src/bin/lex_bin.rs
@@ -4,7 +4,14 @@ use compier::lexer::{Lexer};
 
 fn main() {
     let mut src = String::new();
-    io::stdin().read_to_string(&mut src).unwrap();
+
+    match io::stdin().read_to_string(&mut src) {
+        Ok(_) => {}
+        Err(e) => {
+            eprintln!("Failed to read stdin: {}", e);
+            std::process::exit(1)
+        }
+    }
 
     println!("src string: {}", src);
     let mut lx = Lexer::new(src);
