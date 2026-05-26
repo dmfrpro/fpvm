d6c9a68965acf509416b904a160f3090f739937d
Vladimir <karasev.vladimir.2000@gmail.com>
2026-05-26 11:38:45 +0300

    Add tests examples for for codegen

--- tests/code_extra/README.md
+++
@@ -1,6 +0,0 @@
-# Run and get symbol table output
-
-```
-cargo run --bin dump_sb_tool -- tests/table_check/code
-```
----
---
+++ tests/codegen_test.rs
@@ -0,0 +1,33 @@
+
+#[test]
+fn codegen_golden_branching() {
+}
+
+
+#[test]
+fn codegen_golden_eval() {
+}
+
+#[test]
+fn codegen_golden_func_lambda() {
+}
+
+#[test]
+fn codegen_golden_literals() {
+}
+
+#[test]
+fn codegen_golden_prog() {
+}
+
+#[test]
+fn codegen_golden_quote() {
+}
+
+#[test]
+fn codegen_golden_setq() {
+}
+
+#[test]
+fn codegen_golden_misc() {
+}
