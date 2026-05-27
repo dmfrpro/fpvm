751393f99962a10914dce969c57157f57aafa05b
Ilnur <Ilnur_ha@mail.ru>
2026-05-27 12:45:02 +0300

    fixed setq in the same scope for the same variables; fixed: allowed return applications in prog nodes

--- src/semantics/analyzer.rs
+++ src/semantics/analyzer.rs
@@ -171,15 +171,8 @@ impl SemanticAnalyzer {
                         defined_at: id_node.span.clone(),
                         kind: SymbolKind::Variable,
                     };
-                    if let Err(existing) = self.symbol_table.insert(name.clone(), info) {
-                        self.error(
-                            SemanticErrorKind::DuplicateDefinition(name.clone()),
-                            id_node.span.clone(),
-                            Some(format!(
-                                "variable '{}' already defined at {}",
-                                name, existing.defined_at
-                            )),
-                        );
+                    if let Err(_) = self.symbol_table.insert(name.clone(), info) {
+                        // ignore duplicate definition
                     }
                     self.visit_node(expr);
                 } else {
@@ -235,7 +228,9 @@ impl SemanticAnalyzer {
             }
             NodeKind::ProgNode(_, body_node) => {
                 self.symbol_table.enter_scope();
+                self.push_context(true, self.is_inside_loop());
                 self.visit_node(body_node);
+                self.pop_context();
                 self.symbol_table.exit_scope();
             }
             NodeKind::CondNode(cond, then_expr, else_opt) => {
